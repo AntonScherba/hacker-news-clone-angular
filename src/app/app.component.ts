a39e6916f3552e00c1d266618965385ac64963a4
AntonScherba <antonscherba95@gmail.com>
2021-06-07 11:49:42 +0300

    Revert "Feature/loader"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { delay } from 'rxjs/operators';
-import { LoadingService } from './services/loading.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'hacker-news-clone-angular';
-  isLoading: boolean = true;
-
-  constructor(private loadingService: LoadingService) {}
-
-  ngOnInit() {
-    this.listenToLoading();
-  }
-
-  // Listen to the loadingSub property in the LoadingService class. This drives the
-  // display of the loading spinner.
-  listenToLoading(): void {
-    this.loadingService.loadingSub
-      .pipe(delay(0)) // This prevents a ExpressionChangedAfterItHasBeenCheckedError for subsequent requests
-      .subscribe((loading) => {
-        this.isLoading = loading;
-      });
-  }
 }
